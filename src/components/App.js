c259be456d6e3662900ed61bcd9cc6f99bb911d6
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-12-26 12:23:34 +0000

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div>
       <p>
-        The temperature: <span style={{ color: temperatureColor }}>{weather.temperature}</span>
+        Temperature: <span style={{ color: temperatureColor }}>{weather.temperature}</span>
       </p>
     </div>
   );
